f732248f1ae71c50c0d03dba1a81e9e21d85e385
akbrother02 <akbrother02@gmail.com>
2018-06-29 15:47:19 +0530

    add : Project : make Route

---
+++ app/src/main/java/aasheesh/com/makeroute/MainActivity.java
@@ -0,0 +1,49 @@
+package aasheesh.com.makeroute;
+
+import android.graphics.Point;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        LatLng origin = new LatLng(-7.788969, 110.338382);
+        LatLng destination = new LatLng(-7.781200, 110.349709);
+        DrawRouteMaps.getInstance(this)
+                .draw(origin, destination, mMap);
+        DrawMarker.getInstance(this).draw(mMap, origin, R.drawable.ic_launcher_background, "Origin Location");
+        DrawMarker.getInstance(this).draw(mMap, destination, R.drawable.ic_launcher_foreground, "Destination Location");
+
+        LatLngBounds bounds = new LatLngBounds.Builder()
+                .include(origin)
+                .include(destination).build();
+        Point displaySize = new Point();
+        getWindowManager().getDefaultDisplay().getSize(displaySize);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, displaySize.x, 250, 30));
+    }
+}
